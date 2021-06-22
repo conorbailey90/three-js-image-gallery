9e6b878729b256af19150d995d9e18c10c0c9829
conorbailey90 <conbailey90@gmail.com>
2021-06-22 20:46:16 +0100

    Implemented touch screen functionality

--- js/app.js
+++ js/app.js
@@ -10,13 +10,31 @@ import composerFragment from './shaders/composerFragment.glsl';
 
 import images from './images';
 
+window.mobileCheck = function() {
+    let check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
+
+  let isMobile = window.mobileCheck()
+  alert(isMobile)
+
+
 const grid = document.querySelector('.grid');
+grid.style.transform = `translate3d(0px, 0px, 0)`;
 const gridItems = [...document.querySelectorAll('.grid-item')];
 
 // set background on all images
+// gridItems.forEach((item, idx) => {
+//     let image = document.createElement('img');
+//     image.src = images[idx]
+//     item.appendChild(image);
+// })
+
 gridItems.forEach((item, idx) => {
-    item.style.backgroundImage = `url(./${images[idx]})`
+   item.style.backgroundImage = images[idx]
 })
+
 
 function lerp(start, end, t){
     return start * ( 1 - t ) + end * t;
@@ -29,17 +47,65 @@ const mouse = {
     y: null
 }
 
-window.addEventListener('mousemove', (e) => {
-    // subtracting height and width / 2 so we can translate grid dive negatively
-    mouse.x = e.clientX - (window.innerWidth / 2);
-    mouse.y = e.clientY - (window.innerHeight / 2)
-})
+const touch = {
+    translateX: 0,
+    translateY:0,
+    startX: null,
+    startY: null,
+    gridStartX: 0,
+    gridStartY: 0,
+
+    endX: 0,
+    endY: 0,
+    gridX: 0,
+    gridY: 0
+}
+
+if(isMobile){
+    window.addEventListener('touchstart', (e) =>{
+        touch.translateY = 0;
+        touch.translateX =0;
+        touch.startX = e.touches[0].clientX;
+        touch.startY = e.touches[0].clientY;
+        touch.gridStartX = parseInt(grid.style.transform.split('(')[1].split(',')[0].slice(0,- 2));
+        touch.gridStartY = parseInt(grid.style.transform.split('(')[1].split(',')[1].slice(0,- 2));
+    })
+    
+    window.addEventListener('touchmove', (e)=>{
+        touch.translateX =  (touch.startX - e.touches[0].clientX) * 2.5 ;
+        touch.translateY =  (touch.startY - e.touches[0].clientY ) * 2.5;
+    })
+    
+}else{
+    window.addEventListener('mousemove', (e) => {
+        // subtracting height and width / 2 so we can translate grid dive negatively
+        mouse.x = e.clientX - (window.innerWidth / 2);
+        mouse.y = e.clientY - (window.innerHeight / 2)
+    })
+}
+
+function touchTranslateGrid(){
+   
+     grid.style.transform = `translate3d(${touch.gridX}px, ${touch.gridY}px, 0)`;
+    let currentX = parseInt(grid.style.transform.split('(')[1].split(',')[0].slice(0,- 2));
+    let currentY = parseInt(grid.style.transform.split('(')[1].split(',')[1].slice(0,- 2))
+    touch.gridX = lerp(currentX, touch.gridStartX - touch.translateX, 0.045);
+    touch.gridY = lerp(currentY, touch.gridStartY - touch.translateY, 0.045);
+
+    // Stop grid from being translated out of bounds
+    if(touch.gridX > window.innerWidth * 1.2) touch.gridX = window.innerWidth * 1.2
+    if(touch.gridX < -(window.innerWidth * 1.2)) touch.gridX = -(window.innerWidth * 1.2)
+    if(touch.gridY > window.innerHeight * 1.6) touch.gridY = window.innerHeight * 1.6
+    if(touch.gridY < -(window.innerHeight * 1.6)) touch.gridY = -(window.innerHeight * 1.6)
+  
+}
 
 let currentX = 0;
 let currentY = 0;
 
 // This function is repeatidly called by the webGL class render method.
 function translateGrid(){
+    
     currentX = lerp(currentX, mouse.x, 0.015);
     currentY = lerp(currentY, mouse.y, 0.015);
     grid.style.transform = `translate3d(${-currentX * 3}px, ${-currentY *3}px, 0)`;
@@ -50,6 +116,7 @@ function translateGrid(){
 class WebGL{
     constructor(){
         this.container = document.querySelector('main');
+        // this.gridItems = [...document.querySelectorAll('.grid-item > img')];
         this.gridItems = [...document.querySelectorAll('.grid-item')];
         this.meshItems = [];
         this.scene = new THREE.Scene();
@@ -131,7 +198,12 @@ class WebGL{
         for(let i = 0; i < this.meshItems.length; i++){
             this.meshItems[i].render();
         }
-        translateGrid();
+        if(isMobile){
+            touchTranslateGrid()
+        }else{
+            translateGrid();
+        }
+        // translateGrid();
         // this.renderer.render(this.scene, this.camera)
         this.composer.render()
         requestAnimationFrame(this.render.bind(this));
